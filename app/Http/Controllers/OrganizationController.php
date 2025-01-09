23874595ba57a3bf28063cccb7a6f64509a891f9
Andrey Fedotov <protected.for@gmail.com>
2025-01-09 17:54:59 +0600

    clean up unused methods in controllers

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -28,30 +28,6 @@ class OrganizationController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreOrganizationRequest $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(Organization $organization)
@@ -89,29 +65,5 @@ class OrganizationController extends Controller
             $ids = array_merge($ids, $this->getAllActivityIds($child));
         }
         return $ids;
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Organization $organization)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateOrganizationRequest $request, Organization $organization)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Organization $organization)
-    {
-        //
     }
 }
